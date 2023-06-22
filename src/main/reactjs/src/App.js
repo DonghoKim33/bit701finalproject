84f91f2d317e47c01578786d0eb999f8be08dd3e
DonghoKim33 <124513071+DonghoKim33@users.noreply.github.com>
2023-06-22 12:37:27 +0900

    newcom

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import img1 from './img1.jpeg';
 function App() {
   return (
     <div className="App">
-      <img className={'App-logo'} src={img1} width={300} border={"1"}/>
+      <img className={'App-logo'} alt='' src={img1} width={300} border={"1"}/>
       <br/><br/>
       <h2>Docker 배포 성공 기원 !!!</h2>
     </div>
